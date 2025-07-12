d0c8388673dab9377640b83cec3b91ae3e51ae24
lonehegelian <thelonehegelian@protonmail.com>
2025-07-12 15:20:33 +0100

    feat: Enhance embedding and reranking capabilities
    This commit introduces several significant enhancements to the SymbiontCLI application, focusing on improving flexibility, adding new features, and increasing robustness.
    
    Key changes include:
    
    - Added support for Jina and VoyageAI embedding models, configurable via environment variables.
    - Implemented a flexible reranking system that allows users to choose between Cohere and HuggingFace rerankers.
    - Introduced the ability to process a list of questions from a file, enabling batch question answering.
    - Enhanced logging by saving search results to a file for persistent review and analysis.
    - Updated dependencies to their latest versions, ensuring access to the latest features and security patches.
    - Improved error handling, particularly during the document embedding process, to prevent crashes and provide better feedback.
    - Updated the .gitignore file to exclude generated content such as logs and search results.

--- symbiont_cli/main.py
+++ symbiont_cli/main.py
@@ -17,12 +17,14 @@ from dotenv import load_dotenv
 from pydantic import SecretStr
 from langchain.callbacks import get_openai_callback
 from langchain_voyageai import VoyageAIEmbeddings
+from langchain_community.embeddings import JinaEmbeddings
 from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from langchain.retrievers.document_compressors import CrossEncoderReranker
 
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain_cohere import CohereRerank
 from tqdm import tqdm
+import time
 
 load_dotenv()
 # Initialize colorama
@@ -50,32 +52,17 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-os.environ["COHERE_API_KEY"] = "Uxs5yOSpf9BN2Wamt0qn3uOIKYaGyMhmNDvdcYnO"
+load_dotenv()
 
 
 def init_reranker():
-    if os.environ.get("COHERE_API_KEY") != "":
+    if os.environ.get("RERANKER") == "cohere":
         logger.info("Using Cohere Reranker")
         return CohereRerank(model="rerank-english-v3.0", top_n=10)
-    else:
+    elif os.environ.get("RERANKER") == "huggingface":
         logger.info("Using HuggingFace CrossEncoder Reranker")
-
-        # model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-base")
-        # return CrossEncoderReranker(model=model, top_n=10)
-
-
-compressor = init_reranker()
-
-
-def init_reranker():
-    if os.environ.get("COHERE_API_KEY") != "":
-        logger.info("Using Cohere Reranker")
-        return CohereRerank(model="rerank-english-v3.0", top_n=10)
-    else:
-        logger.info("Using HuggingFace CrossEncoder Reranker")
-
-        # model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-base")
-        # return CrossEncoderReranker(model=model, top_n=10)
+        model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-base")
+        return CrossEncoderReranker(model=model, top_n=10)
 
 
 compressor = init_reranker()
@@ -85,7 +72,7 @@ class SymbiontCLI:
     def __init__(self):
         load_dotenv()
         self.api_key = os.environ.get("OPENAI_API_KEY")
-        self.llm_name = "gpt-4o"
+        self.llm_name = os.environ.get("LLM_NAME", "gpt-4o-mini")
         logger.critical(f"Using LLM: {self.llm_name}")
         if not self.api_key:
             raise ValueError("Please set the OPENAI_API_KEY environment variable")
@@ -131,6 +118,12 @@ class SymbiontCLI:
             default="search_results",
             help="Directory to save search results",
         )
+        parser.add_argument(
+            "--q_list",
+            type=str,
+            default=None,
+            help="File containing list of questions to ask",
+        )
 
         args = parser.parse_args()
         if not os.path.isdir(args.docs_directory):
@@ -146,30 +139,42 @@ class SymbiontCLI:
                 return OpenAIEmbeddings()
             else:
                 raise ValueError("Please set the OPENAI_API_KEY environment variable")
-        if os.environ.get("EMBEDDINGS_MODEL", "").lower() == "sentence-transformers":
-            model_id = "sentence-transformers/all-MiniLM-L6-v2"
+        if "huggingface" in os.environ.get("EMBEDDINGS_MODEL", "").lower():
+            model_id = os.environ.get("EMBEDDINGS_MODEL")
+            # model_id = "sentence-transformers/all-MiniLM-L6-v2"
             model_kwargs = {"device": "cpu"}
             return HuggingFaceEmbeddings(model_name=model_id, model_kwargs=model_kwargs)
-        if os.environ.get("EMBEDDINGS_MODEL", "").lower() == "voyage":
+        if "voyage" in os.environ.get("EMBEDDINGS_MODEL", "").lower():
             return VoyageAIEmbeddings(
                 voyage_api_key=os.environ.get("EMBEDDINGS_MODEL_API_KEY"),
-                model="voyage-3-lite",
+                model=os.environ.get("EMBEDDINGS_MODEL", "voyage-3-lite"),
+                batch_size=8,
             )  # type: ignore
+        if "jina" in os.environ.get("EMBEDDINGS_MODEL", "").lower():
+            api_key = os.environ.get("EMBEDDINGS_MODEL_API_KEY")
+            return JinaEmbeddings(
+                jina_api_key=api_key,
+                model_name="jina-embeddings-v2-base-en",
+                session="default",
+            )
         return HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
+            model_name=os.environ.get(
+                "EMBEDDINGS_MODEL", "sentence-transformers/all-MiniLM-L6-v2"
+            )
+            # model_name="sentence-transformers/all-MiniLM-L6-v2"
         )
 
     def setup_vector_store(self):
         if not self.client.collection_exists(collection_name=self.args.collection_name):
             try:
                 logger.info("Creating collection...")
-                vector_size = None
-                if isinstance(self.embeddings, HuggingFaceEmbeddings):
-                    vector_size = 384
+                vector_size = os.getenv("VECTOR_SIZE")
+                if isinstance(self.embeddings, JinaEmbeddings):
+                    vector_size = 768
                 elif isinstance(self.embeddings, OpenAIEmbeddings):
                     vector_size = 1536
-                elif isinstance(self.embeddings, VoyageAIEmbeddings):
-                    vector_size = 512
+                # elif isinstance(self.embeddings, VoyageAIEmbeddings):
+                #     vector_size = 1024
                 if vector_size is None:
                     raise ValueError("Unknown vector size")
                 self.client.create_collection(
@@ -180,10 +185,11 @@ class SymbiontCLI:
                 )
                 loader = DirectoryLoader(
                     self.args.docs_directory,
-                    glob="**/*.pdf",
+                    # glob="**/*.pdf",
                     show_progress=True,
                     loader_cls=PyMuPDFLoader,
                     silent_errors=True,
+                    use_multithreading=True,
                 )
 
                 documents = loader.load()
@@ -197,7 +203,12 @@ class SymbiontCLI:
                 )
                 with tqdm(total=len(documents), desc="Adding documents") as pbar:
                     for doc, uuid in zip(documents, uuids):
-                        vector_store.add_documents(documents=[doc], ids=[uuid])
+                        try:
+                            if isinstance(self.embeddings, JinaEmbeddings):
+                                time.sleep(0.01)
+                            vector_store.add_documents(documents=[doc], ids=[uuid])
+                        except Exception as e:
+                            logger.error(f"Error adding document with ID {uuid}: {e}")
                         pbar.update(1)
 
             except Exception as e:
@@ -246,6 +257,29 @@ class SymbiontCLI:
             chain_type_kwargs={"prompt": custom_prompt},
         )
 
+    def log_search_results_to_file(
+        self, results, query, response="LLM response is turned off"
+    ):
+        # TODO fix file path issues
+        logs_directory = os.path.join("logs")
+        if not os.path.isdir(logs_directory):
+            os.makedirs(logs_directory)
+
+        file_name = f"{self.args.collection_name}.txt"
+        with open(os.path.join(logs_directory, file_name), "a") as f:
+            f.write(f"Query: {query}\n")
+            f.write(f"LLM Response: \n {response}\n")
+
+            f.write("\n" + "=" * 40 + "\n")
+            for doc in results:
+                f.write("Document Metadata:\n")
+                f.write(f"Source: {doc.metadata['source']}, ")
+                f.write(f"Title: {doc.metadata['title']}, ")
+                f.write(f"Page: {doc.metadata['page']} ")
+                f.write(f"Relevance: {doc.metadata.get('relevance_score', 'N/A')}\n")
+                # f.write("\n" + self.__remove_next_line(doc.page_content) + "\n")
+                f.write("\n" + "=" * 40 + "\n")
+
     def print_search_results(self, results):
         for doc in results:
             self.context += self.__remove_next_line(doc.page_content) + " "
@@ -266,22 +300,41 @@ class SymbiontCLI:
             )
             results = compression_retriever.invoke(query)
             self.print_search_results(results[::-1])
+
             if self.args.llm_response.lower() == "no":
+                self.log_search_results_to_file(results, query)
                 return
             with get_openai_callback() as cb:
                 response = self.qa_stuff.run({"context": self.context, "query": query})
                 logger.critical("\n" + str(cb))
                 logger.info("\n" + response)
+            self.log_search_results_to_file(results, query, response)
         except Exception as e:
             logger.error(f"Error during search and QA: {e}")
 
+    def generate_qa_list(self, questions):
+        with open(questions, "r") as f:
+            questions = f.readlines()
+
+        for q in questions:
+            time.sleep(5)
+            self.perform_search_and_qa(q)
+
+    def query_loop(self):
+        while True:
+            query = input("Enter your query (or type 'exit' to stop): ")
+            if query.lower() == "exit":
+                break
+            self.perform_search_and_qa(query)
+
     def run(self):
         try:
-            while True:
-                query = input("Enter your query (or type 'exit' to stop): ")
-                if query.lower() == "exit":
-                    break
-                self.perform_search_and_qa(query)
+            if self.args.q_list:
+                print("Running questions from file...")
+                self.generate_qa_list(self.args.q_list)
+            else:
+                self.query_loop()
+
         except KeyboardInterrupt:
             logger.info("Exiting gracefully...")
         except Exception as e:
